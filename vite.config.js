e9e8ca48d09d85b6f5aaf07ddb0d44eee0285506
NiyiHub <niyiblack@outlook.com>
2025-09-16 01:00:49 +0100

    Fixed bugs in pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/pages': path.resolve(__dirname, './src/pages'),
+      '@/services': path.resolve(__dirname, './src/services'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/constants': path.resolve(__dirname, './src/constants'),
+      '@/assets': path.resolve(__dirname, './src/assets')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
